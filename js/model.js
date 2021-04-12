a9cacd20df73e4701d24c37a7b696e2fe9933a62
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-04-12 20:50:55 +0200

    added js

---
+++ js/model.js
@@ -0,0 +1,12 @@
+const productName ='Дроид';
+const pricePerItem = 3500;
+const message=`Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов.`;
+console.log(message);
+
+
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee; 
+const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
