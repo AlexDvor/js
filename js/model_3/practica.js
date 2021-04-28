2fcadb94bc463d14091548a2c6fa66a159523622
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-04-28 16:53:14 +0200

    added new rols

--- js/model_3/practica.js
+++ js/model_3/practica.js
@@ -1,7 +1,41 @@
 
+const num = ['1', '2', '3', '4', '5', '6', '7', '8',];
+const lit = ['Coca cola', 'Sprite', 'Name', 'FirstName', 'Family', 'Country', 'London', 'Paris'];
+
+const all = [...num, ...lit];
+// console.log(all);
+console.log({ ...lit });
 // ------------------------------------------------------------------------------------------------------------------------------------------
 
+
+
+// Деструктуризацыя обьекта
+
+// const playlist = {
+//     nameType: "My favorite playlist",
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3', 'track-4'],
+//     tracksCount: 3,
+// };
+
+// const key = Object.values(playlist);
+// console.log(key);
+
+
+// const {
+//     nameType,
+//     rating,
+//     tracks,
+//     tracksCount: trackOfNumber,
+// } = playlist;
+
+// // console.log(nameType, rating, tracks, tracksCount);
+// console.log(trackOfNumber);
+
+
 // -----------------------
+
+
 
 // ------------------------------------------------------------------------------------------------------------------------------------------
 
