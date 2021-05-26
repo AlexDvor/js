f9b5fe8a5fe3b6a798bfa50570557f685ebe07b8
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-05-26 22:46:37 +0200

    added

--- js/model_3/hw.js
+++ js/model_3/hw.js
@@ -695,7 +695,25 @@
 
 //     console.log(countProps(obj));
 
-// ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   const keys = Object.keys(object);
+  
+//   for (const key of keys) {
+//     propCount +=1
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------
 
 
 // Метод Object.keys()
