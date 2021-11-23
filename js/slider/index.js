f617783f696884d1cc99893b3412dfb78bd15c75
Oleksii <alexmatvichuk@gmail.com>
2021-11-23 20:51:40 +0100

    add slider

---
+++ js/slider/index.js
@@ -0,0 +1,20 @@
+let offset = 0; // position from left edge
+const sliderLineRef = document.querySelector('.slider-line');
+const nextBtnRef = document.querySelector('.slider-next');
+const prevBtnRef = document.querySelector('.slider-prev');
+
+nextBtnRef.addEventListener('click', () => {
+  offset += 256;
+  if (offset > 1792) {
+    offset = 0;
+  }
+  sliderLineRef.style.left = -offset + 'px';
+});
+
+prevBtnRef.addEventListener('click', () => {
+  offset -= 256;
+  if (offset < 0) {
+    offset = 1792;
+  }
+  sliderLineRef.style.left = -offset + 'px';
+});
