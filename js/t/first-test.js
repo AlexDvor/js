dcb79029ff6291e108c37759b5585249f66ac68b
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-04-01 20:29:30 +0200

    add new fun

--- js/t/first-test.js
+++ js/t/first-test.js
@@ -1,16 +1,38 @@
-const chooseOptimalDistance = (s, arr) => {
-  const subArray = creatSubarray(arr, s);
+const chooseOptimalDistance = (distance, quantity, list) => {
+  const subArray = creatSubarray(list, quantity);
+  const totalSumArr = totalAmountArray(subArray);
+  const result = compareDistance(totalSumArr, distance);
 
-  return subArray;
+  return result;
 };
 
 function creatSubarray(arr, size) {
   let subarray = [];
-  for (let i = 0; i <= size; i += 1) {
+
+  for (let i = 0; i < arr.length; i += 1) {
     subarray.push(arr.slice(i, i + size));
   }
+  const result = subarray.filter(item => {
+    if (item.length === size) return item;
+  });
 
-  return subarray;
+  return result;
 }
-console.log(chooseOptimalDistance(2, [51, 56, 58, 59, 61])); //173)
-// console.log(chooseOptimalDistance([51, 56, 58, 59, 61])); //173)
+
+function totalAmountArray(arr) {
+  const result = arr.map(items => {
+    return items.reduce((acc, value) => acc + value, 0);
+  });
+
+  return result;
+}
+
+function compareDistance(arr, goal) {
+  const result = arr.reduce((prev, curr) => {
+    return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
+  });
+
+  return result;
+}
+
+console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61])); //173)
