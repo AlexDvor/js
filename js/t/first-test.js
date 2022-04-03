2c949d94b25348f20b2f03b2b3c6d3cb9dbe06d8
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-04-03 15:27:34 +0200

    save

--- js/t/first-test.js
+++ js/t/first-test.js
@@ -1,10 +1,11 @@
 const chooseOptimalDistance = (distance, quantity, list) => {
-  if (list.length < quantity) return null;
+  // if (list.length < quantity) return null;
   const subArray = creatSubarray(list, quantity);
-  // const totalSumArr = totalAmountArray(subArray);
   // const result = compareDistance(totalSumArr, distance);
+  const result = test(list);
+  const a = totalAmountArray(result);
 
-  return result;
+  return subArray;
 };
 
 function creatSubarray(arr, size) {
@@ -13,11 +14,12 @@ function creatSubarray(arr, size) {
   for (let i = 0; i < arr.length; i += 1) {
     subArray.push(arr.slice(i, i + size));
   }
-  const result = subArray.filter(item => {
-    if (item.length === size) return item;
-  });
-  console.log('creatSubarray', result);
-  return result;
+
+  // const result = subArray.filter(item => {
+  //   if (item.length === size) return item;
+  // });
+  // console.log('creatSubarray', result);
+  return subArray;
 }
 
 function totalAmountArray(arr) {
@@ -36,4 +38,28 @@ function compareDistance(arr, goal) {
   return result;
 }
 
-console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61])); //173)
+function test(arr) {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        // console.log('curr', curr);
+        let next = curr.splice(i, 1);
+        // console.log('next', next);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+
+  permute(arr);
+
+  return result;
+}
+
+// console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61])); //173)
+console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59])); //173)
+// console.log(chooseOptimalDistance(174, 3, [1, 2, 3, 4])); //173)
